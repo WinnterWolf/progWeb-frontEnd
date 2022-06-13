308735569d680db38a10700ce32d0efd49f020c7
WinnterWolf <patrick.winter@outlook.com.br>
2022-06-12 21:44:53 -0300

    searchStores screen completed

--- vue.config.js
+++ vue.config.js
@@ -1,31 +1,37 @@
 module.exports = {
   pages: {
     home: {
-      entry: './src/pages/home/main.js',
-      template: 'public/index.html',
-      title: 'Home',
-      chunks: ['chunk-vendors', 'chunk-common', 'home']
+      entry: "./src/pages/home/main.js",
+      template: "public/index.html",
+      title: "Home",
+      chunks: ["chunk-vendors", "chunk-common", "home"],
     },
     login: {
-      entry: './src/pages/login/main.js',
-      template: 'public/index.html',
-      title: 'Login',
-      chunks: ['chunk-vendors', 'chunk-common', 'login']
+      entry: "./src/pages/login/main.js",
+      template: "public/index.html",
+      title: "Login",
+      chunks: ["chunk-vendors", "chunk-common", "login"],
     },
     signupcompany: {
-      entry: './src/pages/signupcompany/main.js',
-      template: 'public/index.html',
-      title: 'SignupCompany',
-      chunks: ['chunk-vendors', 'chunk-common', 'signupcompany']
+      entry: "./src/pages/signupcompany/main.js",
+      template: "public/index.html",
+      title: "SignupCompany",
+      chunks: ["chunk-vendors", "chunk-common", "signupcompany"],
     },
     signupperson: {
-      entry: './src/pages/signupperson/main.js',
-      template: 'public/index.html',
-      title: 'SignupPerson',
-      chunks: ['chunk-vendors', 'chunk-common', 'signupperson']
-    }
-  }
-}
+      entry: "./src/pages/signupperson/main.js",
+      template: "public/index.html",
+      title: "SignupPerson",
+      chunks: ["chunk-vendors", "chunk-common", "signupperson"],
+    },
+    searchstores: {
+      entry: "./src/pages/searchstores/main.js",
+      template: "public/index.html",
+      title: "SignupPerson",
+      chunks: ["chunk-vendors", "chunk-common", "searchstores"],
+    },
+  },
+};
 
 // const { defineConfig } = require("@vue/cli-service");
 // module.exports = defineConfig({
