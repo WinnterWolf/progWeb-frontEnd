28f8a7e5a017cf164e647d6471799df7ab31fbd1
Rafael Guardiano <rafa-guardiano1995@hotmail.com>
2022-06-27 01:45:49 -0400

    Melhorando a tela

--- src/pages/food/App.vue
+++ src/pages/food/App.vue
@@ -15,20 +15,33 @@
       </ul>
     </header>
 
-    <main class="signupbox">
-      <section>
-        <form class="itens_navbar"></form>
+    <main class="foodbox">
+      <div class="left">
+        <div class="div_img">
+          <p>FOTO DO PRATO</p>
+        </div>
+        <div>
+          <h5>DESCRIÇÃO DO PRATO</h5>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero
+            officia libero dicta ad possimus impedit dolor suscipit facilis,
+            voluptate sapiente neque error debitis ex quidem tenetur officiis
+            ipsum labore odit!
+          </p>
+        </div>
+      </div>
 
-        <img
-          src="../../assets/logo.png"
-          alt="comida"
-          width="250"
-          height="200"
-        />
-      </section>
-      <input type="text" id="inf" placeholder="INFORMAÇÕES SOBRE O PRATO" />
-
-      <input class="items_navbar" type="submit" id="submit" value="Confirmar" />
+      <div class="right">
+        <div>
+          <input type="text" placeholder="TITULO" />
+          <input type="text" placeholder="VALOR" />
+        </div>
+        <div>
+          <input type="number" placeholder="QTD" />
+          <button>ADICIONAR QUANTIDADE</button>
+        </div>
+      </div>
+      <button>CONFIRMAR</button>
     </main>
 
     <footer>
@@ -43,7 +56,7 @@
 /*
 Configurar meu css para tela de restaurante - inserir ou editar 
 */
-
+/* eslint-disable */
 html {
   font-size: 62.5%;
 }
@@ -93,7 +106,7 @@ a {
 .items__navbar a {
   text-decoration: none;
   color: #ffffff;
-  font-size: 2rem;
+  font-size: 20px;
 }
 
 .items__navbar h3 {
@@ -103,68 +116,36 @@ a {
   margin-left: 10rem;
 }
 
-.signupbox {
+.right {
+  display: flex;
+  flex-direction: column;
+}
+
+.left {
+  display: flex;
+  flex-direction: column;
+}
+
+.div_img {
+  width: 650px;
+  height: 300px;
+  background: #a0a0a0;
+}
+
+.div_img p {
+  text-align: center;
+  vertical-align: center;
+}
+
+.foodbox {
   display: flex;
   align-items: center;
   justify-content: space-evenly;
   flex-direction: column;
-  width: 100rem;
-  height: 50rem;
-  background: #a0a0a0;
-  border-radius: 1rem;
-}
-
-.inputs {
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  width: 80rem;
-  height: 35rem;
-  flex-wrap: wrap;
-  column-gap: 5rem;
-}
-
-.inputs input {
+  width: 1300px;
+  height: 730px;
   background: #c4c4c4;
-  width: 30rem;
-  height: 3rem;
-  border-radius: 0.5rem;
-  border-style: none;
-  outline: none;
-  padding: 0.5rem;
-  font-size: 1.8rem;
-  color: black;
-}
-
-#category {
-  width: 40rem;
-  height: 5rem;
-  text-align: center;
-}
-
-#submit {
-  background: #00919a;
-  width: 20rem;
-  height: 7rem;
-  border-radius: 0.5rem;
-  border-style: none;
-  outline: none;
-  padding: 0.5rem;
-  font-size: 1.5rem;
-  color: white;
-}
-
-.inputs input::placeholder {
-  color: black;
-}
-
-.loginbox div {
-  text-align: center;
-}
-
-.loginbox div div {
-  margin-top: 5rem;
+  border-radius: 10px;
 }
 
 footer {
